152af6f47ecef0c878e074c45ee64c34b632b26e
Tiago Souza <tiago@etc.digital>
2021-07-05 22:27:25 -0300

    fix: apply some default options

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,27 @@ import puppeteer, { PDFOptions } from "puppeteer";
 const defaultOptions: PDFOptions = {
 	width: 8.3,
 	height: 11.7,
-	headerTemplate: "",
-	footerTemplate: "",
-	format: "a4",
-	margin: { bottom: 10, left: 10, right: 10, top: 10 } /* in pixel, maybe? */,
 	landscape: false,
 	printBackground: false,
 	scale: 1,
+	format: "a4",
+	pageRanges: "1",
 };
 
 const generatePdf = async (
 	html: string,
-	options: PDFOptions = defaultOptions,
+	options?: PDFOptions,
 	puppeteerArgs: string[] = [],
 ): Promise<Buffer> => {
 	const args = ["--no-sandbox", "--disable-setuid-sandbox", ...puppeteerArgs];
 
-	const browser = await puppeteer.launch({ args: args });
+	const browser = await puppeteer.launch({ args: args, headless: true });
 	const page = await browser.newPage();
 
 	await page.setContent(html);
 
-	const data = await page.pdf(options);
+	const opt = { ...defaultOptions, ...options };
+	const data = await page.pdf(opt);
 	await browser.close();
 
 	return Buffer.from(data);
